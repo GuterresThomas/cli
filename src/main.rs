6e8766d1ae36f84c5d0656bc388fd5f3f0d14fe8
Guterresthomas <guterresmikael@gmail.com>
2023-10-03 10:15:55 -0300

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,71 @@
-use clap::Parser;
+use ::clap::{Parser, Subcommand, Args};
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+mod api;
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+#[derive(Parser)]
+#[command(author, version)]
+#[command(about = "stringer - a simple CLI to transform and inspect strings", long_about = "stringer is a super fancy CLI (kidding)
+
+One can use stringer to modify or inspect strings straight from the terminal")]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Reverses a string
+    Reverse(Reverse),
+    /// Inspects a string
+    Inspect(Inspect),
+}
+
+#[derive(Args)]
+struct Reverse {
+    /// The string to reverse
+    string: Option<String>,
+}
+
+#[derive(Args)]
+struct Inspect {
+    /// The string to inspect
+    string: Option<String>,
+    #[arg(short = 'd', long = "digits")]
+    only_digits: bool,
 }
 
 fn main() {
-    let args = Args::parse();
+    let cli = Cli::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    match &cli.command {
+        Some(Commands::Reverse(name)) => {
+            match name.string {
+                Some(ref _name) => {
+                    let reverse = api::stringer::reverse(_name);
+                    println!("{}", reverse);
+                }
+                None => {
+                    println!("Please provide a string to reverse");
+                }
+            }
+        }
+        Some(Commands::Inspect(name)) => {
+            match name.string {
+                Some(ref _name) => {
+                    let (res, kind) = api::stringer::inspect(_name, name.only_digits);
+
+                    let mut plural_s = "s";
+                    if res == 1 {
+                        plural_s = "";
+                    }
+
+                    println!("{:?} has {} {}{}.", _name, res, kind, plural_s);
+                }
+                None => {
+                    println!("Please provide a string to inspect");
+                }
+            }
+        }
+        None => {}
     }
 }
